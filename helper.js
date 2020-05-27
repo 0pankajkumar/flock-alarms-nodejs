220b63f911982572684468f24fe52c913257d640
805bluebell <805.bluebell@gmail.com>
2020-05-27 11:20:31 +0530

    forget axios now

--- helper.js
+++ helper.js
@@ -5,24 +5,26 @@ var axios = require('axios');
 // Get JSON of all contact names
 exports.getContacts = (token) => {
 	 var url = 'https://api.flock.co/v1/roster.listContacts?token=' + token;
-	 axios.all([
-	  axios.get(url)
-	]).then(axios.spread((response1) => {
-	  // console.log(response1.data);
-	  let rawData = response1.data;
-	  let ans = rawData.map(x => {
-	  	return {
-	  		name : x.firstName + ' ' + x.lastName,
-	  		id : x.id,
-	  		profileImage: x.profileImage
-	  	};
-	  });
-	  return [{status : 'yippee'}];
-	  console.log("Length of ans ", ans.length);
+	 
+	//  axios.all([
+	//   axios.get(url)
+	// ]).then(axios.spread((response1) => {
+	//   // console.log(response1.data);
+	//   let rawData = response1.data;
+	//   let ans = rawData.map(x => {
+	//   	return {
+	//   		name : x.firstName + ' ' + x.lastName,
+	//   		id : x.id,
+	//   		profileImage: x.profileImage
+	//   	};
+	//   });
+	//   console.log("Length of ans ", ans.length);
 
-	})).catch(error => {
-	  console.log(error);
-	});
+	// })).catch(error => {
+	//   console.log(error);
+	// });
+
+	  return [{status : 'yippee'}];
 }
 
 
