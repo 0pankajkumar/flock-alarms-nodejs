795545007d476c49f6ea1ec5399e3a5b0bab57a7
805bluebell <805.bluebell@gmail.com>
2020-05-28 16:18:59 +0530

    Setting up the entire form

--- public/scripts.js
+++ public/scripts.js
@@ -1,22 +1,28 @@
 
 var contacts = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        prefetch: '/getContacts'
-    });
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: '/getContacts'
+});
 
-    contacts.initialize();
+contacts.initialize();
 
-    $('.typeahead').typeahead(
-        null, {
-        name: 'stocks',
-        displayKey: 'name',
-        source: contacts.ttAdapter()
-    }).on('typeahead:selected', function(event, data){            
-        $('.typeahead').val(data.code); 
-        console.log(data);
-        document.getElementById('sltd').innerHTML = data.id;
-    });
+$('.typeahead').typeahead(
+    null, {
+    name: 'stocks',
+    displayKey: 'name',
+    source: contacts.ttAdapter()
+}).on('typeahead:selected', function(event, data){            
+    $('.typeahead').val(data.code); 
+    console.log(data);
+    document.getElementById('sltd').innerHTML = data.id;
+});
     
 
+$('#trigger').click(() => {
+    let user = $('#sltd').val();
+    let datetime = $('#datetime').val();
+    let msg = $('#msg').val();
 
+    console.log(user, datetime, msg);
+});
