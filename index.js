b04d7a04263b7d83dc9e4ce0a4a21e9f3d6b856d
805bluebell <805.bluebell@gmail.com>
2020-05-31 17:06:57 +0530

    blinding everything

--- index.js
+++ index.js
@@ -73,22 +73,22 @@ flock.events.on('app.uninstall', function (event, callback) {
 app.get('/submitAlaramRequest', (req, res) => {
    console.log('GET data', req.params);
    res.send('Yo we love it');
-   var r = parseDate(req.param('theDate'));
-    console.log('parse result', r);
-    if (r) {
-        var alarm = {
-            userId: req.param('userId'),
-            time: r.date.getTime(),
-            msg: req.param('msg').slice(r.end).trim()
-        };
-        console.log('adding alarm', alarm);
-        addAlarm(alarm);
-        // callback(null, { text: 'Alarm added' });
-        res.send('submitted');
-    } else {
-        // callback(null, { text: 'Alarm time not specified' });
-        res.send('submission failed ');
-    } 
+   // var r = parseDate(req.param('theDate'));
+   //  console.log('parse result', r);
+   //  if (r) {
+   //      var alarm = {
+   //          userId: req.param('userId'),
+   //          time: r.date.getTime(),
+   //          msg: req.param('msg').slice(r.end).trim()
+   //      };
+   //      console.log('adding alarm', alarm);
+   //      addAlarm(alarm);
+   //      // callback(null, { text: 'Alarm added' });
+   //      res.send('submitted');
+   //  } else {
+   //      // callback(null, { text: 'Alarm time not specified' });
+   //      res.send('submission failed ');
+   //  } 
 });
 
 var parseDate = function (text) {
