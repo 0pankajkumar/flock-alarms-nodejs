7d0381fe849014f38276237cfc65ce6bf9d9c352
805bluebell <805.bluebell@gmail.com>
2020-05-31 17:10:46 +0530

    closing after these

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ flock.events.on('app.uninstall', function (event, callback) {
 
 
 app.get('/submitAlaramRequest', (req, res) => {
-   console.log('GET data', req.params);
-   res.send('Yo we love it');
+   console.log('GET data', req.params.length);
+   res.send(`Yo we love it $(req)`);
    // var r = parseDate(req.param('theDate'));
    //  console.log('parse result', r);
    //  if (r) {
