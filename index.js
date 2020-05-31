06575abdd1078fc80ea57bc8e9f1aab0fe382788
805bluebell <805.bluebell@gmail.com>
2020-05-31 15:16:28 +0530

    consoling more

--- index.js
+++ index.js
@@ -52,11 +52,13 @@ app.listen(process.env.PORT || 8080, function () {
 
 flock.events.on('app.install', function (event, callback) {
     store2.saveToken(event.userId, event.token);
+    console.log('Inserted user probably');
     callback();
 });
 
 flock.events.on('app.uninstall', function (event, callback) {
     store2.deleteToken(event.userId, event.token);
+    console.log('deleted user probably');
     callback();
 });
 
