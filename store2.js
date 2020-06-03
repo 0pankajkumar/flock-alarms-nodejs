bd1572a54c98ba2bd255e72c0d175496e4411c3d
805bluebell <805.bluebell@gmail.com>
2020-06-03 23:00:06 +0530

    will this work?

--- store2.js
+++ store2.js
@@ -61,23 +61,25 @@ exports.addAlarm = function (alarm) {
     // }));
     // alarms.splice(insertAt, 0, alarm);
 
-    // client.query('INSERT INTO public.postman(toid, fromid, msg, timeofsending) VALUES($1, $2, $3, to_timestamp($4)) RETURNING idx', 
-    // 	[alarm.toid, alarm.fromid, alarm.msg, alarm.timeOfSending], (err, response) => {
-    // 	if(err){
-    // 		throw err;
-    // 	}
-    // 	// console.log('Row',response.rows[0].idx,'added successfully');
-    // 	console.log('Alarm addition', response.rowCount > 0 ? 'succeeded' : 'failed');
-    // 	return response.rows[0].idx;
-    // });
+    let ans = null;
+    client.query('INSERT INTO public.postman(toid, fromid, msg, timeofsending) VALUES($1, $2, $3, to_timestamp($4)) RETURNING idx', 
+    	[alarm.toid, alarm.fromid, alarm.msg, alarm.timeOfSending], (err, response) => {
+    	if(err){
+    		throw err;
+    	}
+    	// console.log('Row',response.rows[0].idx,'added successfully');
+    	console.log('Alarm addition', response.rowCount > 0 ? 'succeeded' : 'failed');
+    	ans = response.rows[0].idx;
+    });
+    return ans;
 
-    try{
-    	const res = await client.query('INSERT INTO public.postman(toid, fromid, msg, timeofsending) VALUES($1, $2, $3, to_timestamp($4)) RETURNING idx', 
-    	[alarm.toid, alarm.fromid, alarm.msg, alarm.timeOfSending]);
-    	return res.rows[0].idx;
-    } catch (err) {
-	  console.log(err.stack)
-	}
+ //    try{
+ //    	const res = await client.query('INSERT INTO public.postman(toid, fromid, msg, timeofsending) VALUES($1, $2, $3, to_timestamp($4)) RETURNING idx', 
+ //    	[alarm.toid, alarm.fromid, alarm.msg, alarm.timeOfSending]);
+ //    	return res.rows[0].idx;
+ //    } catch (err) {
+	//   console.log(err.stack)
+	// }
 };
 
 
