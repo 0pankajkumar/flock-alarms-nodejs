b47b896c3b46214b4e0d8cdd576d7daaa85e92f7
805bluebell <805.bluebell@gmail.com>
2020-06-05 14:29:05 +0530

    removed all typeahead code

--- public/scripts.js
+++ public/scripts.js
@@ -1,22 +1,4 @@
 
-var contacts = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    prefetch: '/'
-});
-
-contacts.initialize();
-
-$('.typeahead').typeahead(
-    null, {
-    name: 'stocks',
-    displayKey: 'name',
-    source: contacts.ttAdapter()
-}).on('typeahead:selected', function(event, data){            
-    $('.typeahead').val(data.code); 
-    console.log(data);
-    document.getElementById('sltd').innerHTML = data.id;
-});
     
 
 $('#trigger').click(() => {
