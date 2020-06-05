3a0a30632bf3caea18129a53ba1361c408a3371c
805bluebell <805.bluebell@gmail.com>
2020-06-05 14:35:36 +0530

    thrwing way fromid

--- public/scripts.js
+++ public/scripts.js
@@ -5,14 +5,14 @@ $('#trigger').click(() => {
     let user = document.getElementById('sltd').innerHTML;
     let datetime = Date.parse($('#datetime').val());
     let msg = $('#msg').val();
-    let fromid = document.getElementById('fromid').innerHTML;
+    // let fromid = document.getElementById('fromid').innerHTML;
     let toid = document.getElementById('toid').innerHTML;
     let token = document.getElementById('token').innerHTML;
 
 
     console.log(user, datetime, msg);
 
-    $.get("/submitAlarmRequest", {toid: toid, fromid: fromid, timeOfSend: datetime, msg: msg, token: token}, () => {
+    $.get("/submitAlarmRequest", {toid: toid, fromid: '', timeOfSend: datetime, msg: msg, token: token}, () => {
         document.getElementById('verdict').innerHTML = 'Submitted for lazy sending';
     });
 });
